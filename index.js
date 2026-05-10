cd9b5ee2c025b18e2fd811b71d15c067550f717a
appwebad <appwebprojetct@gmail.com>
2026-05-10 03:34:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:'40px',fontFamily:'Arial'}}>
+      <h1>MedLead Pernambuco</h1>
+      <p>SaaS CRM Médico</p>
+    </div>
+  )
+}
