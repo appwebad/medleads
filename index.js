eae70771ff85bc48ddb8bc3ae00f736610038d71
appwebad <appwebprojetct@gmail.com>
2026-05-10 03:37:20 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:'40px',fontFamily:'Arial'}}>
-      <h1>MedLead Pernambuco</h1>
-      <p>SaaS CRM Médico</p>
-    </div>
-  )
-}
